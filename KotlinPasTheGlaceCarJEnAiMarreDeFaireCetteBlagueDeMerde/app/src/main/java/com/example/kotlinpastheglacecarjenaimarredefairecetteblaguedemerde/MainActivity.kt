81ebbbac67ea4d8199ad7328cafb70b44ae45ef9
Eric Ghoubiguian <ericghoubiguian@live.fr>
2021-05-20 08:40:50 +0200

    Some things change

--- KotlinPasTheGlaceCarJEnAiMarreDeFaireCetteBlagueDeMerde/app/src/main/java/com/example/kotlinpastheglacecarjenaimarredefairecetteblaguedemerde/MainActivity.kt
+++ KotlinPasTheGlaceCarJEnAiMarreDeFaireCetteBlagueDeMerde/app/src/main/java/com/example/kotlinpastheglacecarjenaimarredefairecetteblaguedemerde/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.kotlinpastheglacecarjenaimarredefairecetteblaguedemerde
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val extraitsDeTextes: TextView = findViewById(R.id.extraitsDeTextes)
+        extraitsDeTextes.text = "Jack Malhonette...FBI, CBI, NCIS et tous le bordel !!!!"
     }
 }
