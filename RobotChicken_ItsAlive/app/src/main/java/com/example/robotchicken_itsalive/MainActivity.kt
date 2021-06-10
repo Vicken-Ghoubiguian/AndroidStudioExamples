815be92b2eba8c866d0825c2464148d034f7d71c
Eric Ghoubiguian <ericghoubiguian@live.fr>
2021-06-10 11:05:38 +0200

    United all elements

--- RobotChicken_ItsAlive/app/src/main/java/com/example/robotchicken_itsalive/MainActivity.kt
+++ RobotChicken_ItsAlive/app/src/main/java/com/example/robotchicken_itsalive/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.robotchicken_itsalive
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.speech.tts.TextToSpeech
+import android.widget.Button
+import android.widget.EditText
 
+// Definition of the 'MainActivity' class which define the main activity controller...
 class MainActivity : AppCompatActivity() {
+
+    // Declaration of all elements for this little Android app...
+    private var tts: TextToSpeech? = null
+    private var validationButton: Button? = null
+    private var textToSayEntry: EditText? = null
+
+    // Overriden function to create the main activity...
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // Definition of the main activity...
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //
+
     }
 }
